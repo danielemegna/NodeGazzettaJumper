07b6e04dfbe33e06fb5fbd843181418e6c4dcdde
Daniele Megna <daniele.megna@xpeppers.com>
2016-05-26 14:21:43 +0200

    Developed AvxhomeArticlePage getDownloadLinks

--- src/avxhome/AvxhomeArticlePage.js
+++ src/avxhome/AvxhomeArticlePage.js
@@ -6,19 +6,14 @@ function AvxhomeArticlePage(html) {
   var $cheerioHtml = cheerio.load(html)
 
   this.getDownloadLinks = function() {
-    return [0, 1]
+    return $cheerioHtml('.article .text a[target=_blank]')
+      .map(buildLink)
   }
 
-  var buildLink = function(link) {
+  var buildLink = function(index, link) {
     var cheerioLink = $cheerioHtml(link)
     var text = cheerioLink.text().trim()
     var href = cheerioLink.attr('href')
-
-    if(href.indexOf("avxhome.in") < 0)
-      href = "//avxhome.in" + href
-    if(href.indexOf("http") != 0)
-      href = "http:" + href
-
     return new Link(text, href)
   }
 }
--- tests/avxhome/AvxhomeArticlePageTest.js
+++ tests/avxhome/AvxhomeArticlePageTest.js
@@ -21,6 +21,14 @@ describe('AvxhomeArticlePage', function() {
     it('get download links', function() {
       var links = page.getDownloadLinks()
       expect(links.length).to.equal(2)
+
+      var first = links[0]
+      expect(first.title).to.equal("La Gazzetta Dello Sport con edizioni locali")
+      expect(first.href).to.equal("http://nitroflare.com/view/3D019A7D560A57F")
+
+      var second = links[1]
+      expect(second.title).to.equal("La Gazzetta Dello Sport Rosa con edizioni locali")
+      expect(second.href).to.equal("http://nitroflare.com/view/ECBF7864598CF3B")
     })
     
   })
