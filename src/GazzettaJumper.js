4b81b564e71e88f0b29fa6960d990eab1b706d04
Daniele Megna <megna.dany@gmail.com>
2016-08-16 21:31:39 +0200

    Removed DogList, using not blocking arraylist

--- src/GazzettaJumper.js
+++ src/GazzettaJumper.js
@@ -1,25 +1,28 @@
 var Dog = require('./Dog')
-var DogList = require('./DogList')
 var UserCloudPageFactory = require('./usercloud/UserCloudPageFactory')
 var FilescdnPageFactory = require('./filescdn/FilescdnPageFactory')
 var AvxhomePageFactory = require('./avxhome/AvxhomePageFactory')
 
 function GazzettaJumper(wgetter) {
 
-  var USERCLOUD_ENTER_URL = 'http://userscloud.com/go/k90iwrw5ngla/'
   var FILESCDN_ENTER_URL = 'https://filescdn.com/f/embed/i4t6m655n555'
   var AVXHOME_ENTER_URL = 'http://avxhome.in/newspapers/it'
 
   this.render = function() {
 
-    var dogList = new DogList([
+    var dogList = [
       new Dog(FILESCDN_ENTER_URL, new FilescdnPageFactory(), wgetter),
-      new Dog(USERCLOUD_ENTER_URL, new UserCloudPageFactory(), wgetter),
       new Dog(AVXHOME_ENTER_URL, new AvxhomePageFactory(), wgetter)
-    ])
+    ]
 
-    var link = dogList.find()
-    return '<a href="' + link.href + '">' + link.title + '</a>'
+    var html = "<ul>"
+    for (var i = 0; i < dogList.length; i++) {
+      var link = dogList[i].find()
+      if(link != null)
+        html += '<li><a href="' + link.href + '">' + link.title + '</a></li>'
+    }
+
+    return html + "</ul>"
   }
 }
 
