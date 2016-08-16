f1a09f846e910420da4cf3b634e9d7ae25ae4b85
Daniele Megna <megna.dany@gmail.com>
2016-08-16 22:05:20 +0200

    Improved web gate entrypoint using cache html file

--- web.js
+++ web.js
@@ -1,26 +1,35 @@
 var http = require('http')
+var fs = require('fs');
 var GazzettaJumper = require('./src/GazzettaJumper')
 var RealWGetter = require('./src/RealWGetter')
 
+var HTML_CACHE_FILE = '/tmp/gj.html'
+
 http.createServer(function(req, res) {
-
-  if (req.url === '/favicon.ico') {
-    res.writeHead(200, {'Content-Type': 'image/x-icon'} );
-    res.end();
-    return;
-  }
-
   try { 
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    var wgetter = new RealWGetter()
-    var gj = new GazzettaJumper(wgetter);
-    res.write(gj.render());
+    route(req, res)
   } catch(e) {
-    res.write('No link found! ' + e);
+    res.write('>>> Ops! ' + e);
+  } finally {
+    res.end()
   }
-
-  res.end()
-
 }).listen(80, '0.0.0.0')
+
+function route(req, res) {
+    if (req.url === '/favicon.ico') {
+      res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+      return;
+    }
+
+    if (req.url === '/renew') {
+      var wgetter = new RealWGetter()
+      var gj = new GazzettaJumper(wgetter)
+      var html = gj.render()
+      fs.writeFileSync(HTML_CACHE_FILE, html)
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write(fs.readFileSync(HTML_CACHE_FILE).toString())
+}
 
 console.log('Server running..')
